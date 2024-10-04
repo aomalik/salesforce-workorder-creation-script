f802fc085d93e62f61d665e1f82ed1671226abf7
Adeel Malik <aomalik@live.com>
2024-10-04 18:55:43 +0100

    Verbose message on step 4

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function main() {
             console.log('1. Work Order Ready Platform Event: This will create a Work Order, a Service Appointment, and 2 Work Order Line Items.');
             console.log('2. Service Appointment Ready Platform Event: This will update the Service Appointment status to "Dispatched".');
             console.log('3. Execute Work Order Done Platform Event: This will update the Work Order status to "Done" (Doesnt work yet.');
+            console.log('4. Create two XOi Jobs. Remember to update workordernumber in index.js (Use work order number from step 1)');
             
             const rl = readline.createInterface({
                 input: process.stdin,
